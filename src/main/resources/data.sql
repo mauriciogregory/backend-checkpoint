79c587ade5554a0eb634e512388ff4816c6d2ebd
viniTWL <vinigdc12@gmail.com>
2022-04-19 12:19:51 -0300

    update seeding

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,31 @@
 INSERT INTO categories (name) VALUES ('Violão');
 INSERT INTO categories (name) VALUES ('Bateria');
+INSERT INTO categories (name) VALUES ('Piano e Teclado');
+INSERT INTO categories (name) VALUES ('Cavaquinho');
+INSERT INTO categories (name) VALUES ('Utilitarios');
+INSERT INTO categories (name) VALUES ('Sopro');
+INSERT INTO categories (name) VALUES ('Outros');
 
 INSERT INTO products (title, description, price, image) VALUES
 ('Violão Acústico Preto', 'GIANNINI N-14Bk Violão Acústico Preto', 312.21, 'https://m.media-amazon.com/images/I/419CgWR0hkL._AC_.jpg');
 
 INSERT INTO products (title, description, price, image) VALUES
-('Bateria Acústica', 'Bateria Acústica 2 Tons ESL Prata NY F1rst', 2.289, 'https://m.media-amazon.com/images/I/61opBmhlcqL._AC_SX679_.jpg');
+('Bateria Acústica', 'Bateria Acústica 2 Tons ESL Prata NY F1rst', 2289.90, 'https://m.media-amazon.com/images/I/61opBmhlcqL._AC_SX679_.jpg');
 
 INSERT INTO products (title, description, price, image) VALUES
 ('Clarinete Soprano', 'Baquelite 17 Chave B Chapeamento de Níquel Soprano', 589.00, 'https://m.media-amazon.com/images/I/21taTbkiPOL._AC_US40_.jpg');
 
 INSERT INTO products (title, description, price, image) VALUES
-('Ukulele Soprano', 'Ukulele Soprano', 378.00, 'https://m.media-amazon.com/images/I/61LhvZallaL._AC_SX679_.jpg');
+('Ukulele Soprano', 'Ukulele Spring Us21 Soprano Acústico', 378.00, 'https://m.media-amazon.com/images/I/51TtX6Og9tL._AC_SX679_.jpg');
 
 INSERT INTO products (title, description, price, image) VALUES
-('Piano Digital Alesis', 'Piano Digital Alesis Virtue 88 teclas com pedais, suporte e banco ajustável', 5.100, 'https://m.media-amazon.com/images/I/61lbv4V2lPS._AC_SX679_.jpg"');
+('Piano Digital Alesis', 'Piano Virtue 88 teclas com pedais, suporte e banco ajustável', 5100.00, 'https://m.media-amazon.com/images/I/61lbv4V2lPS._AC_SX679_.jpg"');
 
 INSERT INTO products (title, description, price, image) VALUES
-('Teclado Eletrônico Yamaha', 'TECLADO PIANO MUSICAL ELETRÔNICO YAMAHA PSS-F30', 533.83, 'https://m.media-amazon.com/images/I/61OBGRe1dWS._AC_SX679_.jpg');
+('Teclado Eletrônico Yamaha', 'Teclado Musical Eletrônico PSS-F30', 533.83, 'https://m.media-amazon.com/images/I/61OBGRe1dWS._AC_SX679_.jpg');
 
 INSERT INTO products (title, description, price, image) VALUES
-('Cavaco Rozini Estudante', 'CAVACO ROZINI ESTUDANTE ELETRICO NATURAL RC09ELN', 699.00, 'https://m.media-amazon.com/images/I/51OT7lXN50L._AC_SX679_.jpg');
+('Cavaco Rozini Estudante', 'Cavaco Rozini Elétrico Natural RC09ELN', 699.00, 'https://m.media-amazon.com/images/I/51OT7lXN50L._AC_SX679_.jpg');
 
 INSERT INTO products (title, description, price, image) VALUES
 ('Flauta Doce Soprano', 'Flauta Doce Soprano Barroca C, Yamaha, YRS-24B', 56.75, 'https://m.media-amazon.com/images/I/41YA2rSViNL._AC_SX679_.jpg');
@@ -29,12 +34,51 @@ INSERT INTO products (title, description, price, image) VALUES
 ('Reco-Reco Spanking', 'Reco Reco Spanking 02 molas Alumínio', 68.99, 'https://m.media-amazon.com/images/I/61EB151tMnL._AC_SX679_.jpg');
 
 INSERT INTO products (title, description, price, image) VALUES
-('Pandeiro Light Madeira', 'PANDEIRO CONTEMPORANEA LIGHT 82LT 10 POL MADEIRA NYLON', 166.00, 'https://m.media-amazon.com/images/I/51lgbtiqbYS._AC_SX679_.jpg');
+('Pandeiro Light Madeira', 'Pandeiro Contemporanea Light Pol Madeira Nylon', 166.00, 'https://m.media-amazon.com/images/I/51lgbtiqbYS._AC_SX679_.jpg');
 
 INSERT INTO products (title, description, price, image) VALUES
-('Violão Acústico Clássico', 'Violão Acústico Clássico Nylon C40MII Natural YAMAHA, Yamaha, C40MII', 859.00, 'https://m.media-amazon.com/images/I/61dtUAwnZqL._AC_SX679_.jpg');
+('Violão Acústico Clássico', 'Violão Nylon C40MII Natural, Yamaha, C40MII', 859.00, 'https://m.media-amazon.com/images/I/61yDd24GCVL._AC_SY879_.jpg');
 
 INSERT INTO products (title, description, price, image) VALUES
 ('Violino Vivace Mozart', 'Violino Vivace Mozart Mo44 4/4 Com Case Luxo', 457.00, 'https://m.media-amazon.com/images/I/81dh+FvQUKL._AC_SX679_.jpg');
 
+INSERT INTO products (title, description, price, image) VALUES
+('Afinador Digiral', 'Afinador Tonante Cromático AF10 Preto', 28.90, 'https://m.media-amazon.com/images/I/61SjqEjTb2L._AC_SX679_.jpg');
+
+INSERT INTO products (title, description, price, image) VALUES
+('Cordas Nylon Gianini', 'Encordoamento Violão Nylon GENWB', 22.00, 'https://m.media-amazon.com/images/I/718imtZ65QL._AC_SX679_.jpg');
+
+INSERT INTO products (title, description, price, image) VALUES
+('Capa Violão Clássico', 'Capa Violão Soft Case Clássico Nylon Simples', 59.80, 'https://m.media-amazon.com/images/I/31UHmrusv-L._AC_.jpg');
+
+INSERT INTO products (title, description, price, image) VALUES
+('Capa Cavaquinho', 'Capa Cavaquinho LT BR - Marrom', 184.90, 'https://m.media-amazon.com/images/I/61-Gv6JFhJL._AC_SX679_.jpg');
+
+INSERT INTO products (title, description, price, image) VALUES
+('Palhetas Guitarra', 'Jim Dunlop Palhetas de guitarra acústica (PVP112)', 30.58, 'https://m.media-amazon.com/images/I/81miDgxRirL._AC_SX679_.jpg');
+
+INSERT INTO products (title, description, price, image) VALUES
+('Guitarra Les Paul', 'Guitarra Strinberg Les Paul LPS230 WR', 1505.00, 'https://m.media-amazon.com/images/I/41IOYt3g6qL._AC_SX522_.jpg');
+
+INSERT INTO products (title, description, price, image) VALUES
+('Amplificador Guitarra', 'Cubo para Guitarra GA15II 15W RMS Preto Yamaha', 689.00, 'https://m.media-amazon.com/images/I/61iZPxvECSS._AC_SX679_.jpg');
+
 INSERT INTO productcategory (id_product, id_category) VALUES (1, 1);
+INSERT INTO productcategory (id_product, id_category) VALUES (2, 2);
+INSERT INTO productcategory (id_product, id_category) VALUES (3, 6);
+INSERT INTO productcategory (id_product, id_category) VALUES (4, 7);
+INSERT INTO productcategory (id_product, id_category) VALUES (5, 3);
+INSERT INTO productcategory (id_product, id_category) VALUES (6, 3);
+INSERT INTO productcategory (id_product, id_category) VALUES (7, 4);
+INSERT INTO productcategory (id_product, id_category) VALUES (8, 6);
+INSERT INTO productcategory (id_product, id_category) VALUES (9, 7);
+INSERT INTO productcategory (id_product, id_category) VALUES (10, 7);
+INSERT INTO productcategory (id_product, id_category) VALUES (11, 1);
+INSERT INTO productcategory (id_product, id_category) VALUES (12, 7);
+INSERT INTO productcategory (id_product, id_category) VALUES (13, 5);
+INSERT INTO productcategory (id_product, id_category) VALUES (14, 5);
+INSERT INTO productcategory (id_product, id_category) VALUES (15, 5);
+INSERT INTO productcategory (id_product, id_category) VALUES (16, 5);
+INSERT INTO productcategory (id_product, id_category) VALUES (17, 5);
+INSERT INTO productcategory (id_product, id_category) VALUES (18, 1);
+INSERT INTO productcategory (id_product, id_category) VALUES (19, 5);
